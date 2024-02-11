f5110e4ed60b1d8ae54d00678df6cd60e980e325
Shovan Das <t2dash00@students.oamk.fi>
2024-02-11 17:11:36 +0200

    dropdown menu

--- app/src/main/java/com/example/calories/MainActivity.kt
+++ app/src/main/java/com/example/calories/MainActivity.kt
@@ -4,23 +4,31 @@ import android.os.Bundle
 import android.widget.NumberPicker.OnValueChangeListener
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.selection.selectableGroup
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
 import androidx.compose.material.icons.filled.Email
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -33,6 +41,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.layout.onGloballyPositioned
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
@@ -42,6 +53,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.toSize
 import com.example.calories.ui.theme.CaloriesTheme
 
 class MainActivity : ComponentActivity() {
@@ -64,7 +76,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun CalorieScreen() {
     var weightInput by remember { mutableStateOf("") }
-
+    var male by remember { mutableStateOf(true) }
     Column (
         verticalArrangement = Arrangement.spacedBy(16.dp),
         modifier = Modifier
@@ -74,7 +86,7 @@ fun CalorieScreen() {
         ) {
         Heading(title = stringResource(R.string.calories))
         WeightField(weightInput = weightInput, onValueChange = { weightInput = it })
-
+        GenderChoices(male, setGenderMale = { male = it } )
 
 
 
@@ -127,6 +139,81 @@ fun WeightField(weightInput: String, onValueChange: (String) -> Unit ) {
     )
 }
 
+@Composable
+fun GenderChoices(male: Boolean, setGenderMale: (Boolean) -> Unit) {
+    Column(Modifier.selectableGroup()) {
+        Row (verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(selected = male, onClick = { setGenderMale(true) })
+            Text(text = "Male")
+        }
+        Row (verticalAlignment = Alignment.CenterVertically) {
+            RadioButton(selected = !male, onClick = { setGenderMale(false) })
+            Text(text = "Female")
+        }
+    }
+
+}
+
+@Composable
+fun IntensityList(onClick: (Float) -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+    var selectedText by remember { mutableStateOf("Light") }
+    var textFieldSize by remember { mutableStateOf(Size.Zero) }
+    val items = listOf("Light", "Usual", "Moderate", "Hard", "Very Hard")
+
+    val icon = if (expanded)
+        Icons.Filled.KeyboardArrowUp
+    else
+        Icons.Filled.KeyboardArrowDown
+
+    Column {
+        OutlinedTextField(
+            value = selectedText,
+            readOnly = true,
+            onValueChange = { selectedText = it },
+            label = { Text("Select Intensity") },
+            modifier = Modifier
+                .fillMaxWidth()
+                .onGloballyPositioned { coordinates ->
+                    textFieldSize = coordinates.size.toSize()
+                },
+            trailingIcon = {
+                Icon(
+                    icon,
+                    contentDescription = "Expand",
+                    Modifier.clickable { expanded = !expanded}
+                )
+            }
+
+        )
+
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = { expanded = false },
+            modifier = Modifier.width(with(LocalDensity.current) { textFieldSize.width.toDp() })
+        ) {
+            items.forEach { label ->
+                DropdownMenuItem(
+                    text = { Text(label) },
+                    onClick = {
+                        selectedText = label
+                        expanded = false
+                        var intensity: Float = when (label) {
+                            "Light" -> 1.3f
+                            "Usual" -> 1.5f
+                            "Moderate" -> 1.7f
+                            "Hard" -> 2f
+                            "Very Hard" -> 2.2f
+                            else -> 0.0f
+                        }
+                        onClick(intensity)
+                    }
+                )
+            }
+        }
+    }
+
+}
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
